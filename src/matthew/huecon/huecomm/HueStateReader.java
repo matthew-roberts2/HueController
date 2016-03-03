8c48f1545a430b02bc5a4169298312d988a28aba
matthew-roberts2 <matthewpr2020@gmail.com>
2016-03-03 10:00:35 -0600

    Update HueStateReader.java
    Edited some comments to make them javadoc-able

--- src/matthew/huecon/huecomm/HueStateReader.java
+++ src/matthew/huecon/huecomm/HueStateReader.java
@@ -18,7 +18,7 @@ public class HueStateReader {
 		isCreated = true;
 	}
 	
-	/*
+	/**
 	 * Builds the class based on the given IP
 	 */
 	public static void create(String ip){
@@ -27,19 +27,19 @@ public class HueStateReader {
 		}
 		System.out.println("Instance of HueStateReader created.");
 	}
-	/*
+	/**
 	 * Returns an instance of this class
 	 */
 	public static HueStateReader getHueReader(){
 		return reader;
 	}
-	/*
+	/**
 	 * Returns a boolean based on whether or an instance of the class has been created
 	 */
 	public static boolean hasBeenCreated(){
 		return isCreated;
 	}
-	/*
+	/**
 	 * Used to obtain information from the hue bulb. Cast the output to the expected type
 	 */
 	public Object getProperty(String name, int light) throws Exception{
@@ -61,26 +61,27 @@ public class HueStateReader {
 			return false;
 		}
 	}
-	/*
+	/**
 	 * Wrapper method. Returns a boolean representing whether light number 'light' is on
 	 */
 	public boolean isLightOn(int light) throws Exception{
 		return (boolean) getProperty("on", light);
 	}
-	/*
+	/**
 	 * Wrapper method. Returns a long representing the hue of light number 'light'
 	 */
 	public long getHue(int light) throws Exception{
 		return (long) getProperty("hue", light);
 	}
-	/*
+	/**
 	 * Wrapper method. Returns a long representing the brightness of light number 'light'
 	 */
 	public long getBrightness(int light) throws Exception{
 		return (long) getProperty("bri", light);
 	}
-	/*
+	/**
 	 * Wrapper method. Returns a long representing the saturation of light number 'light'
+	 * @return a long value refering to the bulb's saturation value
 	 */
 	public long getSaturation(int light) throws Exception{
 		return (long) getProperty("sat", light);
